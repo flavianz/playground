4cf4481f3e1ad763a671d2a85e7757154ce1d8d6
flavi <flavianzuellig@hotmail.com>
2024-12-31 00:55:59 +0100

    add home screen

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,20 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { createHashRouter, RouterProvider } from "react-router-dom";
+import Home from "./sites/Home/Home.tsx";
+import TicTacToe from "./sites/TicTacToe/TicTacToe.tsx";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const router = createHashRouter([
+    {
+        path: "/",
+        element: <Home />,
+    },
+    { path: "/tic-tac-toe", element: <TicTacToe /> },
+]);
+
+createRoot(document.getElementById("root")!).render(
+    <StrictMode>
+        <RouterProvider router={router} />
+    </StrictMode>,
+);
---
+++ src/sites/Home/Home.tsx
@@ -0,0 +1,63 @@
+import styles from "./Home.module.css";
+import { Link } from "react-router-dom";
+
+export default function Home() {
+    const games = [
+        {
+            url: "/tic-tac-toe",
+            text: "Tic Tac Toe",
+        },
+        {
+            url: "/tic-tac-toe",
+            text: "Tic Tac Toe",
+        },
+        {
+            url: "/tic-tac-toe",
+            text: "Tic Tac Toe",
+        },
+        {
+            url: "/tic-tac-toe",
+            text: "Tic Tac Toe",
+        },
+        {
+            url: "/tic-tac-toe",
+            text: "Tic Tac Toe",
+        },
+        {
+            url: "/tic-tac-toe",
+            text: "Tic Tac Toe",
+        },
+        {
+            url: "/tic-tac-toe",
+            text: "Tic Tac Toe",
+        },
+        {
+            url: "/tic-tac-toe",
+            text: "Tic Tac Toe",
+        },
+        {
+            url: "/tic-tac-toe",
+            text: "Tic Tac Toe",
+        },
+        {
+            url: "/tic-tac-toe",
+            text: "Tic Tac Toe",
+        },
+    ];
+
+    return (
+        <div id={styles.container}>
+            <h1 id={styles.title}>Get to choosing!</h1>
+            <div id={styles.grid}>
+                {games.map((game, key) => {
+                    return (
+                        <Link to={game.url} key={key} id={styles.gameContainer}>
+                            <div className={styles.iframe} />
+                            <h2 className={styles.gameName}>{game.text}</h2>
+                        </Link>
+                    );
+                })}
+            </div>
+        </div>
+    );
+}
